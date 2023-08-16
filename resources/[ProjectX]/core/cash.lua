078bd4397362ccd32d4538684da7ef8084532e0d
Dominic <B1GSt4RBusiness@gmail.com>
2023-08-17 01:42:50 +0200

    hud ui

--- resources/[ProjectX]/core/cash.lua
+++ resources/[ProjectX]/core/cash.lua
@@ -1,19 +1,31 @@
 local accountmoney = 0;
+local dirty = 0;
+local health = 1;
 
 Citizen.CreateThread(function ()
 RegisterCommand("money", function()
   accountmoney = accountmoney + 500
   UpdateMoneyHud(accountmoney)
 end)
+RegisterCommand("dirtymoney", function()
+  dirty = dirty + 50
+  UpdateMoneyHud(dirty)
+end)
+RegisterCommand("health", function()
+  health = health + 10
+  UpdateMoneyHud(health)
+end)
 end)
 
-function UpdateMoneyHud(money)
-  SendNUIMessage({money = money});
+function UpdateMoneyHud(money, dirty, health)
+  data = {cash = money, dirtycash = dirty, health = health}
+  SendNUIMessage(data);
 end
 
 RegisterNetEvent("nMoneyHud:UpdateMoney")
 AddEventHandler("nMoneyHud:UpdateMoney", function(money)
+  data = {cash = money, dirtycash = dirty, health = health}
   UpdateMoneyHud(money)
 end)
 
-UpdateMoneyHud(accountmoney)
+UpdateMoneyHud({cash = accountmoney, dirtycash = dirty, health = health})
--- resources/[ProjectX]/core/hud/main.js
+++ resources/[ProjectX]/core/hud/main.js
@@ -1,4 +1,56 @@
+function updateHud(data) {
+  $('#cash').text(data.cash);
+  $('#dirtycash').text(data.dirtycash);
+  $('#health').text(data.health);
+  $('#cardio').text(data.cardio);
+  $('#food').text(data.food);
+  $('#water').text(data.water);
+  $('#voice').text(data.voice);
+
+  if(data.dirtycash === 0) $('.dirtycash').addClass('hidden');
+  else $('.dirtycash').removeClass('hidden');
+
+  clearColors('.health');
+  clearColors('.food');
+  clearColors('.water');
+  clearColors('.cardio');
+  clearColors('.voice');
+
+  updatePanel('.health', data.health);
+  updatePanel('.food', data.food);
+  updatePanel('.water', data.water);
+  updatePanel('.cardio', data.cardio);
+  updatePanel('.voice', data.voice);
+}
+
+function updatePanel(panel, value){
+  if(value <= 100){
+    $(panel).addClass('green');
+  }
+  if(value <= 80){
+    $(panel).addClass('yellow');
+  }
+  if(value <= 60){
+    $(panel).addClass('orange');
+  }
+  if(value <= 40){
+    $(panel).addClass('indianred');
+  }
+  if(value <= 20){
+    $(panel).addClass('red');
+  }
+}
+
+function clearColors(panel){
+  $(panel).removeClass('red');
+  $(panel).removeClass('indianred');
+  $(panel).removeClass('orange');
+  $(panel).removeClass('yellow');
+  $(panel).removeClass('green');
+}
+
 window.addEventListener('message', function (event){
-  let data = event.data;
-  $('#money').text(data.money);
+  updateHud(event.data);
 });
+
+updateHud({cash: 0, dirtycash: 1, health: 100, voice: 10, food: 100, water: 100, cardio: 100});
