c1b9d839801b35cfb1c2ee86ac85ec601936e698
Dominic <B1GSt4RBusiness@gmail.com>
2023-08-22 21:47:45 +0200

    loading screen

--- resources/[ProjectX]/core/html/loading/main.js
+++ resources/[ProjectX]/core/html/loading/main.js
@@ -1,24 +1,2 @@
-var count = 0;
-var current = 0;
-
-const handler = {
-  startInitFunctionOrder(data){
-    count = data.count;
-  },
-  initFunctionInvoking(data){
-    document.querySelector(".progress").style.left = "0%";
-    document.querySelector(".progress").style.width = ((data.idx / count) * 100) + "%";
-  },
-  startDataFileEntries(data){
-    count = data.count;
-  },
-  performMapLoadFunction(data){
-    ++current;
-    document.querySelector(".progress").style.left = "0%";
-    document.querySelector(".progress").style.width = ((data.idx / count) * 100) + "%";
-  }
-}
-
-window.addEventListener("message", function(e) {
-  (handler[e.data.eventName] || function () {})(e.data)
-});
+let gfg = document.getElementById("audio");
+gfg.volume = 0.075;
